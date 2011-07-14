e474339e702a1e7611474be15941d257f895b65e
Frank Maritato <fmaritato@attinteractive.com>
2011-07-14 11:28:25 -0700

    ignore this for now

--- src/test/java/com/maritatf/wow/ArmoryUtilTest.java
+++ src/test/java/com/maritatf/wow/ArmoryUtilTest.java
@@ -5,9 +5,11 @@ package com.maritatf.wow;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Ignore;
 
 import static org.junit.Assert.*;
 
+@Ignore
 public class ArmoryUtilTest {
 
     private static transient Log log = LogFactory.getLog(ArmoryUtilTest.class);
